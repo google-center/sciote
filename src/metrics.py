2d6d3d5ade70363cef890828c3caab3303a60f9c
Nikita Karamov <nick@karamoff.ru>
2019-03-12 18:06:02 +0300

    Improve metrics methods

--- src/metrics.py
+++ src/metrics.py
@@ -1,32 +1,38 @@
 import re
 
+
+def split_into_sentences(message):
+    delimiters = "?", "!", ".", "\n"
+    regex_pattern = "|".join(map(re.escape, delimiters))
+    arr = re.split(regex_pattern, message)
+    return list(filter(lambda a: a != '', arr))
+
+
 # Spacing before and/or after punctuation
 # if ok           ->  point += 1
 # if before       ->  point += 2
 # if on both side ->  point += 0
 # if nowhere      ->  point += 3
-def spacing_before_after_punctuation(message):
+def spacing_around_punctuation(message):
     result = 0
     punctuation_count = 0
     points = 0
-    i = 0
     for i in range(0, len(message)):
         cur = message[i]
-        if 0 < i < len(message) - 1 and (
-                cur == "." or cur == "," or cur == "?" or cur == "!" or cur == ":" or cur == ";"):
-            punctuation_count += 1
-            if message[i - 1] == " " and message[i + 1] != " ":
-                points += 2
-            elif message[i - 1] != " " and message[i + 1] == " ":
-                points += 1
-            elif message[i - 1] != " " and message[i + 1] != " ":
-                points += 3
+        if cur in [".", ",", "?", "!", ":", ";"]:
+            if 0 < i < len(message) - 1:
+                punctuation_count += 1
+                if message[i - 1] == " " and message[i + 1] != " ":
+                    points += 2
+                elif message[i - 1] != " " and message[i + 1] == " ":
+                    points += 1
+                elif message[i - 1] != " " and message[i + 1] != " ":
+                    points += 3
 
-        elif i > 0 and i == len(message) - 1 and (
-                cur == "." or cur == "," or cur == "?" or cur == "!" or cur == ":" or cur == ";"):
-            punctuation_count += 1
-            if message[i - 1] != " ":
-                points += 1
+            elif i > 0 and i == len(message) - 1:
+                punctuation_count += 1
+                if message[i - 1] != " ":
+                    points += 1
     if punctuation_count != 0:
         result = (points / punctuation_count) / 3
     return result
@@ -34,11 +40,10 @@ def spacing_before_after_punctuation(message):
 
 # Usage of ellipsis
 # return average ellipsis length
-def avg_len_ellipsis(message):
+def avg_ellipsis_length(message):
     result = 0
     length = 0
     count_sequence = 0
-    i = 0
     duration = 1
     for i in range(0, len(message)):
         if i < len(message) - 1 and message[i] == "." and message[i + 1] == ".":
@@ -58,48 +63,49 @@ def avg_len_ellipsis(message):
 # result[0] = "-"
 # result[1] = "–"
 # result[2] = "—"
-def number_different_dashes(message):
+def number_of_dashes(message):
     result = [0, 0, 0]
-    for i in message:
-        if i == "-":
+    for char in message:
+        if char == "-":
             result[0] += 1
-        elif i == "–":
+        elif char == "–":
             result[1] += 1
-        elif i == "—":
+        elif char == "—":
             result[2] += 1
     return result
 
 
 # Usage of all caps
 # return average length of substrings written by caps
-def avg_len_usage_caps(message):
-    caps_summary_len = 0
-    all_words_length = 0
-    delimiters = "?", "!", ".", ",", "\n", ":", ";"
-    regex_pattern = '|'.join(map(re.escape, delimiters))
-    words = re.split(regex_pattern, message)
-    for word in words:
-        all_words_length += len(word)
-        i = 0
-        size = len(word)
-        duration = 1
-        for i in range(0, size):
-            if i < size - 1 and 'А' <= word[i] <= 'Я' and 'А' <= word[i + 1] <= 'Я':
-                duration += 1
-            elif duration > 1:
-                caps_summary_len += duration
-                duration = 1
+def all_caps_avg_length(message):
+    caps_seqs = []
+    tracking = False
+    cur_seq_length = 0
 
-    result = 0
-    if caps_summary_len > 0:
-        result = caps_summary_len / all_words_length
-    return result
+    for char in message:
+        if char.isalpha():
+            if char.isupper():
+                if not tracking:
+                    tracking = True
+                cur_seq_length += 1
+            else:
+                if tracking:
+                    tracking = False
+                    if cur_seq_length > 1:
+                        caps_seqs.append(cur_seq_length)
+                    cur_seq_length = 0
+
+    total_length = 0
+    for seq in caps_seqs:
+        total_length += seq
+
+    return total_length / len(caps_seqs) if len(caps_seqs) != 0 else 0
 
 
 # Usage of single parenthesis smileys
 # result[0] -> avg length of substrings consisting only of ")"
 # result[1] -> average length of substrings consisting only of "("
-def avg_len_brackets_smile(message):
+def smiley_parenthesis_avg_lengths(message):
     result = [0, 0]
     count_happy_brackets_seq = 0
     count_sad_brackets_seq = 0
@@ -134,8 +140,8 @@ def avg_len_brackets_smile(message):
 
 
 # Если строка оканчивается точкой, возвращаем true,  иначе -  false
-def hasDot(str):
-    if str[len(str) - 1] == '.':
+def ends_with_period(sentence):
+    if sentence[len(sentence) - 1] == '.':
         return True
     else:
         return False
@@ -143,70 +149,53 @@ def hasDot(str):
 
 # Делим сообщения на предложения, считаем, сколько раз
 # сообщение начинается с заглавной. Возвращаем отношение
-def capitalLetter(str):
-    delimiters = "?", "!", ".", "\n"
-    regexPattern = "|".join(map(re.escape, delimiters))
-    arr = re.split(regexPattern, str)
-    arr = list(filter(lambda a: a != '', arr))
-    for i, item in enumerate(arr):
-        arr[i] = arr[i].replace(' ', '')
+def capitalized_amount(message):
+    arr = split_into_sentences(message)
     count = 0
     for element in arr:
-        if (element.istitle()):
+        if element[0].isupper():
             count += 1
     return count / len(arr)
 
 
 # Если строка содержит ё, возвращаем true,  иначе -  false
-def hasE(str):
-    if str.find("ё") == -1:
-        return False
-    else:
-        return True
+def uses_yo(message):
+    return "ё" in message or "Ё" in message
 
 
-# Считаем, сколько предложений содержит воскл знак, считаем общее кол-во предложений
+# Считаем, сколько предложений содержит воскл знак, считаем общее кол-во
+# предложений
 # возвращаем отношение
-def exclaMark(str):
-    arr1 = str.split('!')
-    arr1 = list(filter(lambda a: a != '', arr1))
-    delimiters = "?", "!", ".", "\n"
-    regexPattern = "|".join(map(re.escape, delimiters))
-    arr2 = re.split(regexPattern, str)
-    arr2 = list(filter(lambda a: a != '', arr2))
-    if (str[len(str) - 1] == '!'):
-        return (len(arr1)) / len(arr2)
+def exclamation_amount(message):
+    amt = message.count('!')
+    sentences = split_into_sentences(message)
+
+    if message[-1] == '!':
+        return amt / len(sentences)
     else:
-        return (len(arr1) - 1) / len(arr2)
+        return amt / len(sentences)
 
 
 # возвращаем количество ; в сообщении
-def countSemicolon(str):
-    count = 0
-    for i in str:
-        if i == ';':
-            count += 1
-    return count
+def semicolon_amount(message):
+    return message.count(';')
 
 
 # считаем кол-во слов, которые содержат в себе подстроку из повторяющихся букв
 # возвращаем отношение растянутые слова/все слова
-def longLetters(str):
-    arr = str.split()
+def elongated_words(message):
+    arr = message.split()
     arr = list(filter(lambda a: a != '', arr))
     print(arr)
     count = 0
     for element in arr:
-        i = 0
         c = 1
-        while i < len(element) - 1:
-            if (element[i] == element[i + 1]):
+        for i in range(len(element) - 1):
+            if element[i] == element[i + 1]:
                 c += 1
             else:
-                c = 0
+                c = 1
             if c == 3:
                 count += 1
-                continue
-            i += 1
+                break
     return count / len(arr)
-
